8a02f892519a0dd75c9cbc8b314ae0492ad09302
Praveen Kumar <prkumar@dhcp201-104.englab.pnq.redhat.com>
2013-08-06 15:38:40 +0530

    Added Explaination for dupimage

--- dupimage/dupimage.py
+++ dupimage/dupimage.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+
+"""
+
+Duplicate Image scenario
+* If we use timestamp method then it may posible that same image is copied
+some other place.
+* If we validate using filename then it's high changes of file name vary.
+* If we use md5sum check then also there is posiblity that md5sum differ for
+  different extension of same file (.JPG and .GIF)
+* If we assume pictures are only .jpg and .png or .gif format than pypuzzle
+  looks promising.
+
+"""
+
 from sys import exit
 import os
 from os.path import join
@@ -6,32 +20,28 @@ from argparse import ArgumentParser
 from PIL import ImageChops, Image
 
 
-""" This function takes user folder arguments and
-    make a list of image files"""
-
-
 def image_file_list(path_list):
-    image_files = []
+    """ This function takes user folder arguments and
+        make a list of image files"""
     # Getting Image file details
+    image_files = []
     for directory in path_list:
         for root, dirs, files in os.walk(directory):
             for name in files:
                 image_files.append(join(root, name))
     return image_files
 
-""" This function takes image files to process compare """
-
 
 def image_process(image_list):
+    """ This function takes image files to process compare """
     for i in range(len(image_list)):
         for j in range(i + 1, len(image_list)):
             if not image_compare(image_list[i], image_list[j]):
                 print "%s is same as %s" % (image_list[i], image_list[j])
 
-""" This function will use image file to compare """
-
 
 def image_compare(img1, img2):
+    """ This function will use image file to compare """
     image1 = Image.open(img1)
     image2 = Image.open(img2)
     return ImageChops.difference(image1, image2).getbbox()
@@ -45,7 +55,8 @@ if __name__ == "__main__":
     directory_list = parser.parse_args().folder
     if directory_list:
         image_list = image_file_list(directory_list)
-        image_process(image_list)
+        print image_list
+        #image_process(image_list)
     else:
         parser.print_help()
     exit(0)
